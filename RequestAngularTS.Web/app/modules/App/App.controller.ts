7b687848c533b3eac03d50012528a851d311bb1c
Richard Collette <rcollette@yahoo.com>
2015-02-04 08:19:28 -0500

    Missed a couple files

---
+++ RequestAngularTS.Web/app/modules/App/App.controller.ts
@@ -0,0 +1,23 @@
+﻿export interface INavigationMenuItem {
+    name: string;
+    route: any;
+}
+
+export interface IMasterController {
+    toggleMainMenu(): void;
+}
+
+export class MainController implements IMasterController {
+    private $route: ng.route.IRouteService;
+
+    constructor($route: ng.route.IRouteService) {
+        this.$route = $route;
+        angular.forEach(this.$route.routes,(route, key) => {
+        });
+    }
+
+    public toggleMainMenu() {
+    }
+}
+
+angular.module("app").controller("mainController", MainController);
